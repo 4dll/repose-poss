2c01137d59b8bb50321d5c21da743c12eae967a5
4dll <g.3analhosni@gmail.com>
2026-05-19 21:21:57 +0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ export default function App() {
   return (
     <div className="app">
       <nav className="nav no-print">
-        <NavLink to="/" end>
-          Repose Cafe POS
+        <NavLink to="/" end className="brand-link">
+          <img src="/repose-logo.png" alt="Repose Cafe" className="brand-logo" />
+          <span>POS</span>
         </NavLink>
         <NavLink to="/stock">Stock</NavLink>
         <NavLink to="/reports">Reports</NavLink>
